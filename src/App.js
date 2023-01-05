21faac3066edc554f765f0295afb7d543ad95d5b
SG-Swaroop <sgadekar2704@gmail.com>
2023-01-05 21:56:54 +0530

    final commi

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import AllUsers from "./AllUsers";
 import "./App.css";
-import Assignment from "./Assignment";
-import CleanUpEffect from "./CleanUpEffect";
-import DataFetching from "./DataFetching";
-import OnlyOnceEffect from "./OnlyOnceEffect";
-
-import StateAndEffect from "./StateAndEffect";
 
 function App() {
   return (
     <div className="App">
-      {/*<{StateAndEffect */}
-      {/*<OnlyOnceEffect />*/}
-      {/*<CleanUpEffect />*/}
-      {/*<DataFetching />*/}
-      {/*<Assignment />*/}
       <AllUsers />
     </div>
   );
